dee6f18f2c882b7796ad679b38bf8716130d6595
Yamini Tasare <119790229+yaminitasare@users.noreply.github.com>
2024-02-15 14:26:23 +0530

    Update make_discriminator.py

--- make_discriminator.py
+++ make_discriminator.py
@@ -44,7 +44,3 @@ def discriminator_loss(disc_real_output, disc_generated_output):
   generated_loss = loss_object(tf.zeros_like(disc_generated_output), disc_generated_output)
   total_disc_loss = real_loss + generated_loss
   return total_disc_loss
-
-def my_discriminator():
-    disc = Discriminator()
-    return disc
